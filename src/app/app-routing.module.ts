14436cb73388248f7cc9735b8d0eb32a58333a4f
Samet ÇELİKBIÇAK <sametcelikbicak@outlook.com>
2022-02-19 13:46:37 +0300

    👌 IMPROVE: Add route definitions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { component: LoginComponent, path: 'login' },
+  { component: ProfileComponent, path: 'profile' },
+  { path: '**', pathMatch: 'full', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component, VERSION } from '@angular/core';
 export class AppComponent {
   public title = `Angular ${VERSION.major} Guard usage`;
   public activePage = 'Home';
+
+  public changeActivePage(activePage: string): void {
+    this.activePage = activePage;
+  }
 }
