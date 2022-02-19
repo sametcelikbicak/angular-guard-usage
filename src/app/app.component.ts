879e9a354b4c68abf57ebd4eaa15e91ef9dd46e6
Samet ÇELİKBIÇAK <sametcelikbicak@outlook.com>
2022-02-19 13:33:29 +0300

    📦 NEW: Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  public title = `Angular ${VERSION.major} Guard usage`;
+  public activePage = 'Home';
+}
